c5bd7feae7512b2233020d2362107ec6a120e113
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-06-29 20:16:16 +0430

    cmd fix test 2

--- components/CTL.vhd
+++ components/CTL.vhd
@@ -61,6 +61,7 @@ architecture Behavorial of CTL is
         LDIR <= '0';
         INC <= '0';
         RST <= '0';
+        CMD <= '0';
         
         case pr_state is
             when S0 =>
@@ -78,7 +79,6 @@ architecture Behavorial of CTL is
                 when "00" =>
                   if I_ROUTIR(1 downto 0) = "11" then
                     nx_state <= S2;
-                    CMD <= '0';
                   else
                     nx_state <= S3;
                   end if;
